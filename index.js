0e28a079d89f4b381e78cf9827a673f5c0d0793b
Cmendence <chris.mendence@gmail.com>
2023-12-30 10:41:01 -0500

    added index, dependencies

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-<img src="https://unsplash.com/photos/brown-dried-leaves-in-red-and-white-ceramic-mug-yQ0F0hYEApI" alt="black tea" />
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Read data from db.json
+const rawData = fs.readFileSync('db.json');
+const data = JSON.parse(rawData);
+
+// Example endpoint to get all data
+app.get('/api/data', (req, res) => {
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
